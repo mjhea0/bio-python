ede0b6cc307c47a772bee28b2862928f5b2895f9
Michael Herman <hermanmu@gmail.com>
2013-12-15 15:25:12 -0700

    updated .gitignore

--- tests.py
+++ tests.py
@@ -5,11 +5,11 @@ class Problem1TestCase(unittest.TestCase):
     """Tests for `problem1.py`."""
 
     def test_find_fastq_files_recursively(self):
-        """Will this return all files from the sub directories starting with the parent"""
+        """Should return all files from the sub directories"""
         self.assertEqual(findFastqFiles('./sample_files/fastq', '*.fastq'), ['./sample_files/fastq/read1/Sample_R1.fastq', './sample_files/fastq/read2/Sample_R2.fastq'])
 
     def parseFastqSeq(self):
-        """Will this return all files from the sub directories starting with the parent"""
+        """Should return percentage of sequences > 30 vs all sequences"""
         self.assertEqual(parseFastqSeq(['mock-test1.fastq']), 0.428571428571)
 
 if __name__ == '__main__':
