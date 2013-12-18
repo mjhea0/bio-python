ee9500528d94873d489ed212f4fdd0c745c4c881
Michael Herman <hermanmu@gmail.com>
2013-12-18 16:35:00 -0700

    updates

--- problem3REFACTOR.py
+++ problem3REFACTOR.py
@@ -20,29 +20,47 @@
 from Bio import SeqIO
 import csv
 from collections import OrderedDict
-import pprint
-from BCBio import GFF
 
+def parseTSVforChromes(filename):
+    """Parses a TSV file, return an array of [chromosome,position]"""
+    chromosomes = []
+    with open(filename,'rb') as tabbed_file:
+        tabbed_file = csv.reader(tabbed_file, delimiter='\t')
+        for row in tabbed_file:
+            chromosomes.append(row)
+    return chromosomes
 
+def removeListDups(array):
+    """Removes duplicates from an array"""
+    outlist = []
+    for element in array:
+        if element[0] not in outlist:
+            outlist.append(element[0])
+    return outlist
 
-def parseGTF(filename):
-    """convert TSV to dict"""
-     
-    in_file = filename
-    in_handle = open(in_file)
-    for rec in GFF.parse(in_handle):
-        print rec
-    in_handle.close()
+# MUST CONVERT GTF TO DICTIONARY FOR BETTER PERFROMANCE!
 
-
-
+def parseGTF(filename, array, arrayMinusDups):
+    """returns only rows from GTF that match the chromoson from the TSV,
+       then compares that output with the positions and finally returns
+       all rows where the position falls within the range of coordinates"""
+    finalOutput = []
+    with open(filename,'rb') as tabbed_file:
+        tabbed_file = csv.reader(tabbed_file, delimiter='\t')
+        for row in tabbed_file:
+            for chrom in arrayMinusDups:
+                if row[0] == chrom:
+                    for cordRow in array:
+                        if (row[3] <= cordRow[1] <= row[4]):
+                            finalOutput.append(cordRow)
+    return finalOutput
 
 
 
 #### run code ####
 
-# filename1 = "./mock-test3.tsv"
+filename1 = "./mock-test3.tsv"
 filename2 = "./sample_files/gtf/hg19_annotations.gtf"
-# array = parseTSVforChromes(filename1)
-# arrayMinusDups = removeListDups(array)
-print parseGTF(filename2)
+array = parseTSVforChromes(filename1)
+arrayMinusDups = removeListDups(array)
+print parseGTF(filename2, array, arrayMinusDups)
